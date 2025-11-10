6bc67930e388a3b15e57b478bd7be4944308bd23
jacknycz <jack.nycz@gmail.com>
2025-11-10 14:11:58 -0800

    Remove unused MeetTheNodes import from Home component to streamline code and improve readability.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import Hero from '../components/Hero'
-import MeetTheNodes from '../components/MeetTheNodes'
 import FeatureTabsSection from '../components/FeatureTabsSection'
 import ProLanding from '../components/ProLanding'
 import FinalCTASection from '../components/FinalCTASection'
